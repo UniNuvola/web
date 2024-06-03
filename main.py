5954c9f67ce79d7562a72907adec7ccf602f781f
Nicolò Vescera <nicolo.vescera@gmail.com>
2024-06-03 17:10:55 +0200

    🔧 (db.py): create new file db.py and implement a DBManager class with SQLite3 connection
    - ✨db.py, main.py: add import statement for the new db.py file and initialize an instance of DBManager
    - 🌐 templates/user.html: create new user.html template file to display a message and a button for requesting access
    - ✅ main.py: update login route to redirect to '/user' instead of returning "NICE!" after successful authentication

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
 import hvac
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from db import DBManager
 
 
+dbm = DBManager()
 app = Flask(__name__)
 
 
-def client():
-    client = hvac.Client(
-        url='http://127.0.0.1:8200'
-    )
-
-    print(f"Client Auth: {client.is_authenticated()}")
-
-    try:
-        client.auth.userpass.login(
-            username='alice',
-            password='alice'
-        )
-    except:
-        print("Wrong Pass or id")
-        exit(1)
-
-    print(f"Client Auth: {client.is_authenticated()}")
-    print("Autenticato, redirect to ...")
-
-    print(vars(client.auth.userpass))
-    print(client.auth.userpass.read_user('alice'))
+# def client():
+#     client = hvac.Client(
+#         url='http://127.0.0.1:8200'
+#     )
+#
+#     print(f"Client Auth: {client.is_authenticated()}")
+#
+#     try:
+#         client.auth.userpass.login(
+#             username='alice',
+#             password='alice'
+#         )
+#     except:
+#         print("Wrong Pass or id")
+#         exit(1)
+#
+#     print(f"Client Auth: {client.is_authenticated()}")
+#     print("Autenticato, redirect to ...")
+#
+#     print(vars(client.auth.userpass))
+#     print(client.auth.userpass.read_user('alice'))
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -46,12 +48,19 @@ def login():
     except:
         return "Wrong Pass or id"
 
-    return "NICE!"
+    # return "NICE!"
+    return redirect('/user')
+
+
+@app.route('/user')
+def user():
+    return render_template('user.html')
 
 
 @app.route("/")
 def hellow_world():
     return render_template('index.html')
+
 # print(vars(client.session))
 # print(vars(client))
 # print(client.read("/auth/userpass/users/alice"))
