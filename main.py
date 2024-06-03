1abb06d6262f63fb8735fbbc17949d35c539654f
Nicolò Vescera <nicolo.vescera@gmail.com>
2024-06-03 15:44:59 +0200

    🌐 (main.py, pyproject.toml, templates/index.html): create new files for Flask web application
    WebApp basic structure and dependencies setup:
     - login page and form,
     - hvac and flask imports,
     - routing configuration.

---
+++ main.py
@@ -0,0 +1,59 @@
+import hvac
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+
+def client():
+    client = hvac.Client(
+        url='http://127.0.0.1:8200'
+    )
+
+    print(f"Client Auth: {client.is_authenticated()}")
+
+    try:
+        client.auth.userpass.login(
+            username='alice',
+            password='alice'
+        )
+    except:
+        print("Wrong Pass or id")
+        exit(1)
+
+    print(f"Client Auth: {client.is_authenticated()}")
+    print("Autenticato, redirect to ...")
+
+    print(vars(client.auth.userpass))
+    print(client.auth.userpass.read_user('alice'))
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.method != 'POST':
+        return "<h1>ERRORE !</h1>"
+    
+    client = hvac.Client(
+        url='http://127.0.0.1:8200'
+    )
+
+    # print(f"Client Auth: {client.is_authenticated()}")
+
+    try:
+        client.auth.userpass.login(
+            username=request.form['user'],
+            password=request.form['pass']
+        )
+    except:
+        return "Wrong Pass or id"
+
+    return "NICE!"
+
+
+@app.route("/")
+def hellow_world():
+    return render_template('index.html')
+# print(vars(client.session))
+# print(vars(client))
+# print(client.read("/auth/userpass/users/alice"))
+# config_response = client.secrets.activedirectory.read_config()
+# print(config_response)
