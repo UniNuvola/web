adf8e077fa0d937d090e343b36baeebe15426246
Nicolò Vescera <nicolo.vescera@gmail.com>
2024-06-10 10:41:32 +0200

    🔧 Refactor homepage template and login functionality
    - In main.py:
      - Update Vault API base URL (🌐)
      - Add a new test route for user page (💡)
      - Try logout functionality by revoking access token from Vault API (❌)
    
    - In templates/home.html:
      - Update layout to display user information if logged in (🐛)
      - Add a logout button and corresponding functionality (✨)
    
    Now we can get the user infos (email, phone number, ecc.) because in
    Vault must be setted a group, policies and metadata for evry
    identity/user.

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ oauth = OAuth(app)
 oauth.register(
     name='vault',
     server_metadata_url=CONF_URL,
+    api_base_url='http://localhost:8200/v1/',
     client_kwargs={
         'scope': 'openid default'
     }
@@ -22,6 +23,7 @@ oauth.register(
 @app.route('/')
 def homepage():
     user = session.get('user')
+
     return render_template('home.html', user=user)
 
 
@@ -34,13 +36,28 @@ def login():
 @app.route('/auth')
 def auth():
     token = oauth.vault.authorize_access_token()
+
     session['user'] = token['userinfo']
-    return redirect('/')
+    session['token'] = token
 
+    return redirect(url_for('homepage'))
 
+# @app.route('/user')
+# def user():
+#     user = session.get('user')
+#
+#     return f"<h1>EMIAL: {user['contact']['email']}</h1>"
+
+# TODO: logut non funziona
 @app.route('/logout')
 def logout():
+    token = session.get('token')
+
+    oauth.vault.post('auth/token/revoke-self', token=token)
+
     session.pop('user', None)
+    session.pop('token', None)
+
     return redirect('/')
 
 
