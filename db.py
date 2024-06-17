2c671c4ba8eba12aa30a81a9c6d7db54d4b53ca3
Nicolò Vescera <nicolo.vescera@gmail.com>
2024-06-17 18:09:23 +0200

    🌐 (db.py): refactor DBManager class to handle SQLite3
    Refactor DBManager class to support SQLite3 connection pooling and add new methods for adding and getting request status
    Introduce a new method `get_request_status` to retrieve the state of a specific user's request from the database
    Update documentation by adding TODO comments and adding a note about deprecated datetime adapter usage

--- db.py
+++ db.py
@@ -1,34 +1,81 @@
 import sqlite3
+from datetime import datetime
 
 
+# TODO: gestione loggign (passare instanza app ?)
 class DBManager():
     __db_instance = None
     __tab_richieste = 'richieste'
     __dbfile = 'database_richieste.db'
 
     # TODO: introduce a db connection LazyLoad ?
+    # TODO: db name as input or others ?
     def __init__(self):
         if DBManager.__db_instance is None:
             DBManager.__db_instance = sqlite3.connect(DBManager.__dbfile)
 
         self.__check_db()
 
-
     def __check_db(self):
-        # Check if main table exists
-        c = DBManager.__db_instance.cursor()
-        c.execute(f''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='{DBManager.__tab_richieste}' ''')
+        with sqlite3.connect(DBManager.__dbfile) as conn:
+            # Check if main table exists
+            c = conn.cursor()
+            c.execute(f''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='{DBManager.__tab_richieste}' ''')
 
-        # Table exists ! No operation needed
-        if c.fetchone()[0] == 1:
-            return
+            # Table exists ! No operation needed
+            if c.fetchone()[0] == 1:
+                c.close()
+                return
+            
+            c.close()
 
-        # Table do not exists !
-        c = DBManager.__db_instance.cursor()
-        c.execute(f"CREATE TABLE {DBManager.__tab_richieste}(user, start_date, state, end_date)")
+            # Table do not exists !
+            c = conn.cursor()
+            c.execute(f"CREATE TABLE {DBManager.__tab_richieste}(user, start_date, state, end_date)")
 
-        DBManager.__db_instance.commit()
+            conn.commit()
+            c.close()
+
+    def add_request(self, user: str):
+        assert user is not None
+        assert user != ''
+
+        with sqlite3.connect(DBManager.__dbfile) as conn:
+            c = conn.cursor()
+            c.execute(f"SELECT * FROM {DBManager.__tab_richieste} WHERE user = ?", (user,))
+
+            rows = c.fetchall()
+
+            # Prevent saturating DB with a-doc crafted POST request
+            # Only one request is admitted per user
+            if len(rows) != 0:
+                c.close()
+
+                return
+
+            # WARNING:  DeprecationWarning: The default datetime adapter is deprecated as of Python 3.12;
+            #           see the sqlite3 documentation for suggested replacement recipes
+            c.execute(f"INSERT INTO {DBManager.__tab_richieste} VALUES(?, ?, ?, ?)", (user, datetime.now(), 0, None))
+            c.close()
+            conn.commit()
+
+    def get_request_status(self, user: str):
+        assert user is not None
+        assert user != ''
+        
+        with sqlite3.connect(DBManager.__dbfile) as conn:
+            c = conn.cursor()
+            c.execute(f"SELECT * FROM {DBManager.__tab_richieste} WHERE user = ?", (user,))
+
+            rows = c.fetchall()
+            c.close()
+
+            assert len(rows) <= 1
+
+            return rows
 
 if __name__ == "__main__":
-   db = DBManager() 
+    db = DBManager() 
+    db.add_request('alice.alice@alice.it')
+    db.get_request_status("alice.alice@alice.it")
    # db2 = DBManager() 
