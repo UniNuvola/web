1397ac7cd3280f8e3af42a643a01d03f76ad08c9
Nicolò Vescera <nicolo.vescera@gmail.com>
2024-06-19 19:15:22 +0200

    📝 (config.py): add a list of admin users to the configuration file

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ load_dotenv()
 VAULT_CLIENT_ID = os.getenv('VAULT_CLIENT_ID')
 VAULT_CLIENT_SECRET = os.getenv('VAULT_CLIENT_SECRET')
 
+ADMIN_USERS = ['alice.alice@alice.it', 'prova@unipg.it', 'eliasforna@gmail.com']
