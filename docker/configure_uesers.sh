fa6a52eb4e5b30b1fe7324d74269b526d4fa0e52
Nicolò Vescera <nicolo.vescera@gmail.com>
2024-07-01 18:03:44 +0200

    🔧 (docker/configure_uesers.sh): remove script
    Removed script to configure Vault users as it is no longer needed.

--- docker/configure_uesers.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# enalbe userpass auth and create 2 dummy users
-docker exec vault sh -c 'vault login dev-only-token && \
-  vault auth enable userpass && \
-  vault write auth/userpass/users/alice password=alice policies=defaults && \
-  vault write auth/userpass/users/bob password=bob policies=defaults'
